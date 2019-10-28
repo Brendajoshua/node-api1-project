cdb9741dbb8e9404959aa5d3d6e27acb604dbd0a
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-28 17:03:57 +0300

    created `GET` request to `/api/users`

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+
+const db = require('./data/db.js')
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+
+server.get("/api/users", (req, res) => {
+    db.find()
+    .then(users => res.send(users))
+    .catch(error => 
+        res.status(500).json({
+            error: "The users information could not be retrieved."
+        })
+        );
+})
+
+
+server.listen(process.env.PORT || 5000, () => {
+    console.log('listening on' + (process.env.PORT || 5000));
+})
